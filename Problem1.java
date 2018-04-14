35521c54923dba25837bdd80ca145616603b8288
aymane lotfi <31337952+aymane-lotfi@users.noreply.github.com>
2018-04-14 23:29:27 +0100

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java .util.ArrayList;
 public class Main {
     public static void main (String[] args){
-        
-    
+       
+    ArrayList<String> list = new ArrayList();
     Scanner sc = new Scanner (System.in);
     int T = sc.nextInt();    
     while(T!=0){
@@ -11,13 +12,16 @@ public class Main {
                   int w = sc.nextInt();
         for (int i=0;i<n;i++){
         int l = sc.nextInt();
-        if(h * h + w * w <= l * l)
-		System.out.println("NO");
-			else
-              System.out.println("YES");
+        if(h * h + w * w <l * l){
+ 
+        
+		list.add("NO");}
+			else{
+             list.add("YES");}
     }
         T--;
     }
-    
+    for (String str: list) {
+       System.out.println(str);
+		}
 }}
-
